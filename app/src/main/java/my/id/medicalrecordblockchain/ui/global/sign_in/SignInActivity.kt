9607a9f6b3d93c7f03c88f6429673abcfbd97afa
hafiz <hafiz.rahman@bareksa.com>
2024-04-01 22:07:05 +0700

    slicing patient sign up and personal data

--- app/src/main/java/my/id/medicalrecordblockchain/ui/global/sign_in/SignInActivity.kt
+++ app/src/main/java/my/id/medicalrecordblockchain/ui/global/sign_in/SignInActivity.kt
@@ -3,6 +3,7 @@ package my.id.medicalrecordblockchain.ui.global.sign_in
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import my.id.medicalrecordblockchain.databinding.ActivitySignInBinding
+import my.id.medicalrecordblockchain.ui.patient.sign_up.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
@@ -10,5 +11,17 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+        initListener()
+    }
+
+    private fun init() {
+
+    }
+
+    private fun initListener() {
+        binding.tvSignUp.setOnClickListener {
+            SignUpActivity.launch(this)
+        }
     }
 }
--- app/src/main/java/my/id/medicalrecordblockchain/ui/patient/sign_up/SignUpActivity.kt
+++ app/src/main/java/my/id/medicalrecordblockchain/ui/patient/sign_up/SignUpActivity.kt
@@ -1,12 +1,27 @@
 package my.id.medicalrecordblockchain.ui.patient.sign_up
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import my.id.medicalrecordblockchain.R
+import androidx.appcompat.app.AppCompatActivity
+import my.id.medicalrecordblockchain.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    companion object {
+        fun launch(context: Context) {
+            context.startActivity(
+                Intent(
+                    context,
+                    SignUpActivity::class.java
+                )
+            )
+        }
     }
 }
