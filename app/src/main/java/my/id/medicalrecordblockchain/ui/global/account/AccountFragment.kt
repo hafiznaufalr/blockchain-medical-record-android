e4d2adec3bf91d97568a0ca7c2fe9f15d7f3526d
hafiz <hafiz.rahman@bareksa.com>
2024-04-05 04:30:11 +0700

    reuse account fragment

---
+++ app/src/main/java/my/id/medicalrecordblockchain/ui/global/account/AccountFragment.kt
@@ -0,0 +1,6 @@
+package my.id.medicalrecordblockchain.ui.global.account
+
+import androidx.fragment.app.Fragment
+
+class AccountFragment: Fragment() {
+}
--- app/src/main/java/my/id/medicalrecordblockchain/ui/patient/account/AccountPatientFragment.kt
+++
@@ -1,6 +0,0 @@
-package my.id.medicalrecordblockchain.ui.patient.account
-
-import androidx.fragment.app.Fragment
-
-class AccountPatientFragment: Fragment() {
-}
